ecf77c3b58b9c16548e085ff3f4b4bd3348b612e
Slab3 <zshylga@gmail.com>
2020-12-17 23:21:14 +0200

    v 1.3.1 stable

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,8 +2,13 @@ var slider = document.getElementById('slider'),
     sliderItems = document.getElementById('slides'),
     prev = document.getElementById('prev'),
     next = document.getElementById('next'),
-    dotsElem = document.getElementsByClassName('dot');
-
+    dotsElem = document.getElementsByClassName('dotItem'),
+    // dots = document.getElementsByClassName('dots')[0],
+    dot0 = document.getElementById('dot0'),
+    dot1 = document.getElementById('dot1'),
+    dot2 = document.getElementById('dot2'),
+    dot3 = document.getElementById('dot3'),
+    dot4 = document.getElementById('dot4');
 
 function slide(wrapper, items, prev, next) {
     var posX1 = 0,
@@ -37,37 +42,14 @@ function slide(wrapper, items, prev, next) {
     // click events - and DOTS
     prev.addEventListener('click', function () {
         shiftSlide(-1);
-
-        //dots -1
-        for (let i = 0; i < dotsElem.length; i++) {
-            if (index >=0 && index < dotsElem.length){
-                dotsElem[i].classList.remove('active');
-            } else {
-                dotsElem[i].classList.remove('active');
-                dotsElem[dotsElem.length - 1].classList.add('active');
-            }
-        }
-        if (index >= 0 && index < dotsElem.length) {
-            dotsElem[index].classList.add('active');
-        }
+        //dots dir -1
+        dotActiveLast()
     });
     next.addEventListener('click', function () {
         shiftSlide(1);
-
-        //dots 1
-        for (let i = 0; i < dotsElem.length; i++) {
-            if (index >=0 && index < dotsElem.length){
-                dotsElem[i].classList.remove('active');
-            } else {
-                dotsElem[i].classList.remove('active');
-                dotsElem[0].classList.add('active');
-            }
-        }
-        if (index >= 0 && index < dotsElem.length) {
-            dotsElem[index].classList.add('active');
-        }
+        //dots dir 1
+        dotActive()
     });
-
 
 
     // transition events
@@ -123,35 +105,14 @@ function slide(wrapper, items, prev, next) {
             if (dir == 1) {
                 items.style.left = (posInitial - slideSize) + "px";
                 index++;
+                //dots dir swipes 1
+                dotActive()
 
-                //dots w swipes 1
-                for (let i = 0; i < dotsElem.length; i++) {
-                    if (index >=0 && index < dotsElem.length){
-                        dotsElem[i].classList.remove('active');
-                    } else {
-                        dotsElem[i].classList.remove('active');
-                        dotsElem[0].classList.add('active');
-                    }
-                }
-                if (index >= 0 && index < dotsElem.length) {
-                    dotsElem[index].classList.add('active');
-                }
             } else if (dir == -1) {
                 items.style.left = (posInitial + slideSize) + "px";
                 index--;
-
-                //dots w swipes -1
-                for (let i = 0; i < dotsElem.length; i++) {
-                    if (index >=0 && index < dotsElem.length){
-                        dotsElem[i].classList.remove('active');
-                    } else {
-                        dotsElem[i].classList.remove('active');
-                        dotsElem[dotsElem.length - 1].classList.add('active');
-                    }
-                }
-                if (index >= 0 && index < dotsElem.length) {
-                    dotsElem[index].classList.add('active');
-                }
+                //dots dir swipes -1
+                dotActiveLast()
             }
         };
 
@@ -173,6 +134,71 @@ function slide(wrapper, items, prev, next) {
 
         allowShift = true;
     }
+
+
+    // making dot active /else- if "index gon' be > length", go to FIRST slide/
+    function dotActive() {
+        for (let i = 0; i < dotsElem.length; i++) {
+            if (index >=0 && index < dotsElem.length){
+                dotsElem[i].classList.remove('active');
+            } else {
+                dotsElem[i].classList.remove('active');
+                dotsElem[0].classList.add('active');            // active first sl
+            }
+        }
+        if (index >= 0 && index < dotsElem.length) {
+            dotsElem[index].classList.add('active');
+        }
+    }
+
+    // making dot active /else- if "index gon' be< 0", go to LAST slide/
+    function dotActiveLast() {
+        for (let i = 0; i < dotsElem.length; i++) {
+            if (index >=0 && index < dotsElem.length){
+                dotsElem[i].classList.remove('active');
+            } else {
+                dotsElem[i].classList.remove('active');
+                dotsElem[dotsElem.length - 1].classList.add('active'); // active last sl
+            }
+        }
+        if (index >= 0 && index < dotsElem.length) {
+            dotsElem[index].classList.add('active');
+        }
+    }
+
+    // dot eventListeners, changing slides & active dots.
+    //this can be in one eventListener.
+    dot0.addEventListener('click',function () {
+        items.style.left = (-slideSize) + "px";
+        index = 0;
+
+        dotActive()
+    });
+    dot1.addEventListener('click',function () {
+        items.style.left = (-slideSize*2) + "px";
+        index = 1;
+
+        dotActive()
+    });
+    dot2.addEventListener('click',function () {
+        items.style.left = (-slideSize*3) + "px";
+        index = 2;
+
+        dotActive()
+    });
+    dot3.addEventListener('click',function () {
+        items.style.left = (-slideSize*4) + "px";
+        index = 3;
+
+        dotActive()
+    });
+    dot4.addEventListener('click',function () {
+        items.style.left = (-slideSize*5) + "px";
+        index = 4;
+
+        dotActive()
+    });
+
 }
 
 slide(slider, sliderItems, prev, next);
